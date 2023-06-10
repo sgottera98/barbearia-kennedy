d00411497b3676dc0ad03ee3f5dc05efb8327540
Bruno Sgotti <bruno.ferreira@fatecitapetininga.edu.br>
2023-06-10 14:20:58 -0300

    feat: organizacao de partials

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import './App.scss'
+import './App.scss';
 import Hero from './ui/partials/hero';
 import Services from './ui/partials/services';
 import Professionals from './ui/partials/Professionals';
 
 function App() {
-
-  return (
-    <>
-      <div className="bg-default w-screen h-screen text-white">
-        <Hero/>
-      </div>
-      <div className="bg-image w-screen h-auto text-white">
-        <div className='bg-default bg-opacity-80'>
-          <Services/>
-        </div>
-      </div>
-      <div className="bg-default to-black w-screen h-auto text-white">
-          <Professionals/>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="bg-default w-screen min-h-screen text-white">
+                <Hero />
+                <Services />
+                <Professionals />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
