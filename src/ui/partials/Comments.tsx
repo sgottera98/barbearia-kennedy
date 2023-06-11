9f6e309154bc61bb0dd90abf8e2d68a6bc519725
gabrielbueno99 <gabrielpregss@gmail.com>
2023-06-11 17:16:19 -0300

    comments and Professionals

---
+++ src/ui/partials/Comments.tsx
@@ -0,0 +1,28 @@
+import Shave from '../../assets/shave.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+import "swiper/css";
+import "swiper/css/pagination";
+
+const Comments = () => {
+    return (
+        <section className='bg-image'>
+            <div className="bg-gradient mx-auto px-5 md:px-16r flex h-screen">
+                <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{ delay: 5000, disableOnInteraction: false, }}
+                pagination={{clickable: true,}}
+                navigation={true}
+                modules={[Autoplay, Pagination]}
+                className='my-swiper'
+                >
+                    <SwiperSlide className='flex justify-center items-center'>Slide 1</SwiperSlide>
+                    <SwiperSlide className='flex justify-center items-center'>Slide 2</SwiperSlide>
+                </Swiper>
+            </div>
+        </section>
+    )
+}
+
+export default Comments;
--- src/ui/partials/Professionals.jsx
+++ src/ui/partials/Professionals.jsx
@@ -1,45 +1,42 @@
 import Shave from '../../assets/shave.jpg';
-import {Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
 import "swiper/css";
 import "swiper/css/pagination";
 
-const Professionals = ()=> {
+const Professionals = () => {
     return (
-        <section className="container mx-auto px-5 md:px-16 flex justify-center items-center h-screen">
-            <div className="rounded-lg h-screen md:w-1/2 max-h-sm md:py-20 md:px-16">
-                <img className='h-full object-cover rounded-lg' src={Shave} alt="" />
-            </div>
-            <div className="md:w-1/2 m-4 h-1/2 flex items-center">
-                <Swiper
-                direction={"vertical"}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                  }}
-                pagination={{clickable: true,}}
-                modules={[Pagination]}
-                className='w-full h-screen'
-                >
-                    <SwiperSlide className='text-white flex flex items-center justify-center'>
-                        <div className='max-w-md mx-auto bg-black bg-opacity-50 rounded-lg shadow-2xl flex flex-col justify-center items-center'>
-                            <p className='my-4'>Temos os melhores!</p>
-                            <p className='my-4 mx-4'>Cada membro da nossa equipe é treinado profissionalmente e tem anos de experiência na indústria da barbearia.</p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className='text-white flex flex items-center justify-center'>
-                        <div className='max-w-md mx-auto bg-black bg-opacity-50 rounded-lg shadow-2xl flex flex-col justify-center items-center'>
-                            <p className='my-4'>Temos os melhores!</p>
-                            <p className='my-4 mx-4'>Cada membro da nossa equipe é treinado profissionalmente e tem anos de experiência na indústria da barbearia.</p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className='text-white flex flex items-center justify-center'>
-                        <div className='max-w-md mx-auto bg-black bg-opacity-50 rounded-lg shadow-2xl flex flex-col justify-center items-center'>
-                            <p className='my-4'>Temos os melhores!</p>
-                            <p className='my-4 mx-4'>Cada membro da nossa equipe é treinado profissionalmente e tem anos de experiência na indústria da barbearia.</p>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
+        <section className='bg-image'>
+            <div className="bg-gradient-left px-5 md:px-16 flex justify-end items-center h-screen">
+                <div className="md:w-1/2 m-4 h-1/2 flex items-center">
+                    <Swiper
+                        direction={"vertical"}
+                        autoplay={{delay: 5000,}}
+                        loop={true}
+                        pagination={{ clickable: true, }}
+                        modules={[Pagination, Autoplay]}
+                        className='w-full h-screen'
+                    >
+                        <SwiperSlide className='text-white flex flex items-center justify-center'>
+                            <div className='max-w-md mx-4 flex flex-col justify-center items-center text-start'>
+                                <p className='w-full'>Temos os melhores</p>
+                                <p className='my-4'>Cada membro da nossa equipe é treinado profissionalmente e tem anos de experiência na indústria da barbearia.</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className='text-white flex flex items-center justify-center'>
+                            <div className='max-w-md mx-4 flex flex-col justify-center items-center text-start'>
+                                <p className='w-full'>Conhecimento técnico</p>
+                                <p className='my-4'>Nossos profissionais estão sempre aprendendo e acompanhando as últimas tendências em tratamentos masculinos e técnicas modernas de barbear.</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className='text-white flex flex items-center justify-center'>
+                            <div className='max-w-md mx-4 flex flex-col justify-center items-center text-start'>
+                                <p className='w-full'>Atendimento personalizado</p>
+                                <p className='my-4'>Trabalhamos para fornecer a cada cliente o melhor atendimento e tratamento possível. Nos preocupamos com a sua satisfação e para isso estamos sempre prontos para ouví-lo e garantir um serviço personalizado.</p>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
             </div>
         </section>
     )
