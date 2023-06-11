47504bca728479dd681dc905901529869ffc3ab6
Bruno Sgotti <bruno.ferreira@fatecitapetininga.edu.br>
2023-06-11 12:12:23 -0300

    feat: conclusão do hero

--- src/ui/partials/Hero.jsx
+++ src/ui/partials/Hero.jsx
@@ -2,19 +2,25 @@ import BarbershopImage from '../../assets/barbershop.jpg';
 
 const Hero = () => {
     return (
-        <section className="container mx-auto px-5 md:px-16 flex justify-center items-center">
-            <div className="md:w-1/2 md:py-20 md:px-16">
-                <h2>Barbearia Kennedy</h2>
-                <p>
-                    Bem-vindo à Barbearia Kennedy, onde fornecemos o melhor
-                    serviço de barbearia para homens da cidade. De cortes de
-                    cabelo modernos e estilosos a barbas bem cuidadas, temos
-                    tudo para garantir que você saia daqui se sentindo confiante
-                    e pronto para enfrentar o mundo.
-                </p>
-            </div>
-            <div className="md:w-1/2 m-4 h-full">
-                <img src={BarbershopImage} alt="" className='h-full object-cover'/>
+        <section className="container mx-auto px-2 xl:px-48 pb-4">
+            <div className="flex justify-center items-center flex-col-reverse xl:flex-row bg-black xl:bg-default rounded-b-lg">
+                <div className="xl:w-2/3 p-6 xl:py-20 xl:px-16">
+                    <h2>Barbearia Kennedy</h2>
+                    <p>
+                        Bem-vindo à Barbearia Kennedy, onde fornecemos o melhor
+                        serviço de barbearia para homens da cidade. De cortes de
+                        cabelo modernos e estilosos a barbas bem cuidadas, temos
+                        tudo para garantir que você saia daqui se sentindo
+                        confiante e pronto para enfrentar o mundo.
+                    </p>
+                </div>
+                <div className="w-full xl:w-1/3 xl:m-4 overflow-hidden h-[75vh]">
+                    <img
+                        src={BarbershopImage}
+                        alt=""
+                        className="w-full h-full object-cover xl:rounded-lg"
+                    />
+                </div>
             </div>
         </section>
     );
