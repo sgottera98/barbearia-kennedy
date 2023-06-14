3cfcc35a3865cdfebaa1d9b81d23ae30acb22307
Luiz <felipe-f-cunha@hotmail.com>
2023-06-13 22:32:05 -0300

    Pequeno ajuste section sobre nos para monitores 4k

--- src/ui/partials/AboutUs.jsx
+++ src/ui/partials/AboutUs.jsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
         <h2>Sobre Nós</h2>
         <div className="flex flex-col gap-4 md:flex-row">
           <div className="bg-card h-2/4" data-aos="zoom-in">
-            <div className="pt-6 flex justify-center">
+            <div className="pt-6 flex justify-center 2xl:pt-12">
               <img className="w-52 h-52" src={scissor} alt="" />
             </div>
             <Card title={"Tradição em serviços de barbearia"}>
@@ -19,8 +19,12 @@ const AboutUs = () => {
               foco do nosso trabalho diário.
             </Card>
           </div>
-          <div className="bg-card h-2/4" data-aos="zoom-in" data-aos-delay="200">
-            <div className="pt-6 flex justify-center">
+          <div
+            className="bg-card h-2/4"
+            data-aos="zoom-in"
+            data-aos-delay="200"
+          >
+            <div className="pt-6 flex justify-center 2xl:pt-12">
               <img className="w-52 h-52" src={utensils} alt="" />
             </div>
             <Card title={"Tradição em serviços de barbearia"}>
@@ -29,8 +33,12 @@ const AboutUs = () => {
               foco do nosso trabalho diário.
             </Card>
           </div>
-          <div className="bg-card h-2/4" data-aos="zoom-in" data-aos-delay="400">
-            <div className="pt-6 flex justify-center">
+          <div
+            className="bg-card h-2/4"
+            data-aos="zoom-in"
+            data-aos-delay="400"
+          >
+            <div className="pt-6 flex justify-center 2xl:pt-12">
               <img className="w-52 h-52" src={BarbershopImage} alt="" />
             </div>
             <Card title={"Tradição em serviços de barbearia"}>
